b59ee1c9615c425970dd750718078f7a69a440f3
rachit-j <rjaiswal.sd.77@gmail.com>
2023-12-18 01:30:17 -0800

    Hax

--- randomgenerator.py
+++ randomgenerator.py
@@ -1,7 +1,7 @@
 import random
 
 # Generating a list of 1000 random numbers
-numbers = [random.randint(1, 5001) for _ in range(10000)]
+numbers = [random.randint(1, 500) for _ in range(10000)]
 
 # Converting the list to a comma-separated string
 numbers_str = ",".join(map(str, numbers))
